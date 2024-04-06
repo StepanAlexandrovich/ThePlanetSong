ada7a3c20cd9f0acc95b108b459be95f110a3c7a
stepan <fisenko.stepan@mail.ru>
2024-04-06 22:03:05 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(200,200);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
